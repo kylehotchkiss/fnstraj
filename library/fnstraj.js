a24edb1a581a27d1f0660e4365b15b835ba5e66f
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2012-11-28 15:42:53 -0500

    Guess which trajectory predictor can use GFS and RAP now? MINE.
    Added in GFS model support, starting GFSHD. The option to switch them
    isn't in userspace yet, but will autoselect engine in the future, when
    feasible.

--- library/fnstraj.js
+++ library/fnstraj.js
@@ -10,22 +10,22 @@
  * program control here. Flow also flies around async.whilst a bit, which is
  * explained well enough below.
  *
- * fnstraj.vertPred - Quick predictor for time-tracking purposes. 
+ * fnstraj.vertPred - Quick predictor for time-tracking purposes.
  * fnstraj.predict - fnstraj (linear) predictor loop
  *
- */ 
+ */
 var async    = require('async');
 var grads    = require('./grads.js');
 var output   = require('./output.js');
 var physics  = require('./physics.js');
 var position = require('./position.js');
- 
+
 
 
 //////////////////////////////////////
 // Vertical Predictor (Frame Count) //
 //////////////////////////////////////
-exports.vertPred = function( launchAlt, burstAlt, radius, lift ) {    
+exports.vertPred = function( launchAlt, burstAlt, radius, lift ) {
     var table     = [ launchAlt ];
     var flying    = true;
     var status    = "ascending";
@@ -33,16 +33,16 @@ exports.vertPred = function( launchAlt, burstAlt, radius, lift ) {
 
     while ( flying ) {
         if ( status === "ascending") {
-            var ascended = position.ascend( table[ table.length - 1 ], burstAlt, lift, radius );    
+            var ascended = position.ascend( table[ table.length - 1 ], burstAlt, lift, radius );
             var currAlt = ( ascended * 60 ) + altitude;
 
             if ( currAlt >= burstAlt ) {
                 status = "descending";
             } else {
-                altitude = currAlt; 
+                altitude = currAlt;
                 table[ table.length ] = altitude;
             }
-        } else if ( status === "descending") {            
+        } else if ( status === "descending") {
             var descended = 5; //position.descend( table[ frame - 1], 2267, 1.52);
             var currAlt = altitude - ( descended * 60 );
 
@@ -54,7 +54,7 @@ exports.vertPred = function( launchAlt, burstAlt, radius, lift ) {
                 table[ table.length ] = altitude;
             }
         }
-    }    
+    }
 
     return table.length;
 }
@@ -68,81 +68,81 @@ exports.predict = function( flight ) {
     var cache = [],
         stats = { frames: 0, gradsHits: 0, cacheHits: 0 },
         table = [{ latitude: flight.launch.latitude, longitude: flight.launch.longitude, altitude: flight.launch.altitude }];
-        
+
     flight.flying = true;
     flight.status = "ascending";
-        
+
     console.log("\033[1;34m\n FNSTRAJ BALLOON TRAJECTORY PREDICTOR\n\n \033[1;37mGenerating flight path (this will take several minutes)...\n\033[0m ");
-        
+
     async.whilst(
         ////////////////////
-        // LOOP CONDITION // 
+        // LOOP CONDITION //
         ////////////////////
-        function () {            
+        function () {
             return flight.flying;
         },
-        
-        
-        
+
+
+
         //////////////////
         // PRIMARY LOOP //
         //////////////////
-        function ( callback ) { 
+        function ( callback ) {
             var timestep = flight.launch.timestamp + ((table.length - 1 ) * 1000); //ms or s bro
-            
+
             if ( flight.status === "ascending" ) {
                 var ascended = 5;//position.ascend( table[table.length - 1].altitude, flight.balloon.burst, flight.balloon.lift, flight.balloon.radius );
                 var currAlt = ( ascended * 60 ) + table[table.length - 1].altitude;
-                     
+
                 if ( currAlt < flight.balloon.burst ) {
                     table[table.length] = { altitude: currAlt };
-                    
+
                     stats.frames++;
-                    
-                    grads.wind( table[table.length - 2 ], timestep, "rap", table, cache, stats, callback ); // Variable me!
+
+                    grads.wind( table[table.length - 2 ], timestep, "gfs", table, cache, stats, callback ); // Variable me!
                 } else {
                     //
-                    // Burst 
+                    // Burst
                     // Logic flow: transision to descent mode.
                     //
                     flight.points = {
                         burst: { latitude: table[table.length - 1].latitude, longitude: table[table.length - 1].longitude, altitude: table[table.length - 1].altitude }
                     };
-                    
-                    flight.status = "descending";   
-                    
-                    callback(); // Blank return - Yikes. Eval this. 
-                }       
+
+                    flight.status = "descending";
+
+                    callback(); // Blank return - Yikes. Eval this.
+                }
             } else if ( flight.status === "descending" ) {
                 var descended = 5;
                 var currAlt = table[table.length - 1].altitude - ( descended * 60 );
-                    
+
                 if ( currAlt > 0 ) {
                     table[table.length] = { altitude: currAlt };
-                    
+
                     stats.frames++;
-                    
-                    grads.wind( table[table.length - 2 ], timestep, "rap", table, cache, stats, callback ); // Variable me!
+
+                    grads.wind( table[table.length - 2 ], timestep, "gfs", table, cache, stats, callback ); // Variable me!
                 } else {
                     //
                     // Landing
                     // Logic flow: calculation complete, finalize.
                     //
                     flight.flying = false;
-                    
+
                     process.nextTick(function() {
                         output.writeFiles(table, callback);
                     });
                 }
             }
-        },  
-        
-        
-        
+        },
+
+
+
         ///////////////////////////////
         // FINALIZE & ERROR HANDLING //
         ///////////////////////////////
-        function ( error ) {             
+        function ( error ) {
             if ( error != null ) {
                 console.log("Closing");
             }
--- library/grads.js
+++ library/grads.js
@@ -6,88 +6,171 @@
  *
  * grads.wind - return wind speed and direction.
  *
+ * A LOT happens in this file. Most everything, actually. Most things are
+ * explained fairly well below, except for model offsets. These are what help
+ * determine which forecast time to choose. They are based off of the time
+ * difference between model forecast periods.
+ *
  */
+
 var url   = require("url");
 var http  = require("http");
 var async = require('async');
 var position = require('./position.js');
 
 
-
 //
-// If this gets a damn null latitude/longitude HELL NULL ANYTHINg
-// then cut the execution.
+// automatically pick model :P
 //
 
 
 exports.wind = function( frame, time, model, table, cache, stats, parentCallback ) {
-    var lev, u_ext, v_ext;
+    var gfs_hourset, gfs_offset, now, lev, u_ext, v_ext;
     var radians = Math.PI / 180;
     var degrees = 180 / Math.PI;
     var baseURL = "http://nomads.ncep.noaa.gov:9090/dods/";
 
 
 
-    ///////////////////////////
-    // Date/Time Corrections //
-    ///////////////////////////    
-    now    = new Date( time );
-    now.setHours( now.getHours() - 5 ); // Probably timezone related fix?
-    year   = now.getUTCFullYear();
-    month  = ( now.getUTCMonth() < 9 ) ? "0" + ( now.getUTCMonth() + 1 ) : ( now.getUTCMonth() + 1); 
-    date   = ( now.getUTCDate() < 10 ) ? "0" + now.getUTCDate() : now.getUTCDate(); // Check this on the 10th of the month
-    hour   = ( now.getUTCHours() < 10 ) ? "0" + now.getUTCHours() : now.getUTCHours(); // Check this on the 10th hour (utc?)
-    minute = Math.round(( now.getUTCMinutes() / 60) * 18 );
+    now = new Date( time );
+    now.setHours( now.getHours() - 5 ); // Probably timezone related fix? Does this need to choose the timezone? Should we not use GMT below?
 
 
- 
-    ///////////////////////////////////
-    // Coordinate Mapping (to Model) //
-    ///////////////////////////////////
-    if ( model === "rap" ) {
+
+    ////////////////////////////
+    // Lazy null-dreamcatcher //
+    ////////////////////////////
+    for ( var i = 0; i < arguments.length; i++ ) {
+        if ( arguments[i] === null || arguments === NaN ) {
+            console.log("\033[0;31mlogicfail: NULL caught/grads.js.\033[0m");
+
+            parentCallback( true );
+
+            break;
+        }
+
         //
-        // Investigate an *actual* formula for this, champ. Not linear.
-        // 
-        latitude  = Math.floor(( ( frame.latitude - 16.281 ) / ( 58.02525454545 ) ) * 226 ); // WRONG.
-        longitude = Math.floor(( ( -139.85660300000 - ( frame.longitude + 57.69083517955 )) / -139.85660300000 ) * 427 ); // WRONG.
-
-        modelURL = "rap/rap" + year + month + date + "/rap_" + hour + "z.ascii?";
-    } else if ( model === "gfs" ) {
-        // pick right forecast hourset
-        
-        modelURL = "gfs/gfs" + year + month + date + "/gfs_" + hour + "z.ascii?";
-    } else if ( model === "gfshd" ) {
-
+        // quit loop exec via var or something (can't fit in logic flow)
+        //
     }
 
 
 
     //////////////////////////////////
-    // Altitude Level Determination //
+    // Offset Determination for RAP //
     //////////////////////////////////
-    if ( frame.altitude < 12000 ) {
-        level = Math.round(( frame.altitude / 12000 ) * 36); // Does this apply to GFS/GFSHD?
+    var rap_offset = Math.round(( now.getUTCMinutes() / 60) * 18 );
 
-        u_ext = "ugrdprs[" + minute + "][" + level + "][" + latitude + "][" + longitude + "]";
-        v_ext = "vgrdprs[" + minute + "][" + level + "][" + latitude + "][" + longitude + "]";
-    } else if ( frame.altitude >= 12000 && frame.altitude < 14000 ) {
-        u_ext = "ugrd180_150mb[" + minute + "][" + latitude + "][" + longitude + "]";
-        v_ext = "vgrd180_150mb[" + minute + "][" + latitude + "][" + longitude + "]";
-    } else if ( frame.altitude >= 14000 && frame.altitude < 15000 ) {
-        u_ext = "ugrd150_120mb[" + minute + "][" + latitude + "][" + longitude + "]";
-        v_ext = "vgrd150_120mb[" + minute + "][" + latitude + "][" + longitude + "]";
-    } else if ( frame.altitude >= 15000 && frame.altitude < 17000 ) {
-        u_ext = "ugrd120_90mb[" + minute + "][" + latitude + "][" + longitude + "]";
-        v_ext = "vgrd120_90mb[" + minute + "][" + latitude + "][" + longitude + "]";
-    } else if ( frame.altitude >= 17000 && frame.altitude < 19000 ) {
-        u_ext = "ugrd90_60mb[" + minute + "][" + latitude + "][" + longitude + "]";
-        v_ext = "vgrd90_60mb[" + minute + "][" + latitude + "][" + longitude + "]";
-    } else if ( frame.altitude >= 19000 && frame.altitude < 24000 ) {
-        u_ext = "ugrd60_30mb[" + minute + "][" + latitude + "][" + longitude + "]";
-        v_ext = "vgrd60_30mb[" + minute + "][" + latitude + "][" + longitude + "]";
-    } else if ( frame.altitude >= 24000 ) {
-        u_ext = "ugrd30_0mb[" + minute + "][" + latitude + "][" + longitude + "]";
-        v_ext = "vgrd30_0mb[" + minute + "][" + latitude + "][" + longitude + "]";
+
+
+    ///////////////////////////////////////////////
+    // Hourset & Offset Determinaion for GFS(HD) //
+    ///////////////////////////////////////////////
+    if ( model === "gfs" || model === "gfshd" ) {
+        var hour = now.getUTCHours();
+
+        if ( hour < 6 ) {
+            gfs_hourset = 0;
+        } else if ( hour >= 6 && hour < 12 ) {
+            gfs_hourset = 6;
+        } else if ( hour >= 12 && hour < 18 ) {
+            gfs_hourset = 12;
+        } else if ( hour >= 18 && hour < 24 ) {
+            gfs_hourset = 18;
+        }
+
+        gfs_offset = Math.round(( hour - gfs_hourset ) / 3);
+        gfs_hourset = ( gfs_hourset < 10 ) ? "0" + gfs_hourset : gfs_hourset;
+    }
+
+
+
+    ///////////////////////////
+    // Date/Time Corrections //
+    ///////////////////////////
+    var year        = now.getUTCFullYear();
+    var month       = ( now.getUTCMonth() < 9 ) ? "0" + ( now.getUTCMonth() + 1 ) : ( now.getUTCMonth() + 1); // wtf why is this one month in the future
+    var date        = ( now.getUTCDate() < 10 ) ? "0" + now.getUTCDate() : now.getUTCDate(); // Check this on the 10th of the month (appears fine)
+    var hour        = ( now.getUTCHours() < 10 ) ? "0" + now.getUTCHours() : now.getUTCHours(); // Check this on the 10th hour (utc?)
+
+
+
+    ///////////////////////////////////
+    // Coordinate Mapping (to Model) //
+    ///////////////////////////////////
+    if ( model === "rap" ) {
+        latitude  = Math.floor((( frame.latitude - 16.281 ) / ( 58.02525454545 )) * 226 ); // WRONG.
+        longitude = Math.floor((( -139.85660300000 - ( frame.longitude + 57.69083517955 )) / -139.85660300000 ) * 427 ); // WRONG.
+        modelURL = "rap/rap" + year + month + date + "/rap_" + hour + "z.ascii?";
+
+
+
+        //////////////////////////////////
+        // Altitude Level Determination //
+        //////////////////////////////////
+        if ( frame.altitude < 12000 ) {
+            level = Math.round(( frame.altitude / 12000 ) * 36);
+
+            u_ext = "ugrdprs[" + rap_offset + "][" + level + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrdprs[" + rap_offset + "][" + level + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 12000 && frame.altitude < 14000 ) {
+            u_ext = "ugrd180_150mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd180_150mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 14000 && frame.altitude < 15000 ) {
+            u_ext = "ugrd150_120mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd150_120mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 15000 && frame.altitude < 17000 ) {
+            u_ext = "ugrd120_90mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd120_90mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 17000 && frame.altitude < 19000 ) {
+            u_ext = "ugrd90_60mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd90_60mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 19000 && frame.altitude < 24000 ) {
+            u_ext = "ugrd60_30mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd60_30mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 24000 ) {
+            u_ext = "ugrd30_0mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd30_0mb[" + rap_offset + "][" + latitude + "][" + longitude + "]";
+        }
+    } else if ( model === "gfs" ) {
+        latitude  = Math.floor( (( frame.latitude + 180 ) / 360 ) * 180 );
+        longitude = Math.floor( frame.longitude + 180 );
+        modelURL = "gfs/gfs" + year + month + date + "/gfs_" + gfs_hourset + "z.ascii?";
+
+
+
+        //////////////////////////////////
+        // Altitude Level Determination //
+        //////////////////////////////////
+        if ( frame.altitude < 1829 ) {
+            u_ext = "ugrd_1829m[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd_1829m[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 1829 && frame.altitude < 2743 ) {
+            u_ext = "ugrd_2743m[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd_2743m[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 2743 && frame.altitude < 3658 ) {
+            u_ext = "ugrd_3658m[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd_3658m[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+        } if ( frame.altitude >= 3658 && frame.altitude < 25908 ) {
+            level = Math.round(( frame.altitude / 25908 ) * 25);
+
+            u_ext = "ugrdprs[" + gfs_offset + "][" + level + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrdprs[" + gfs_offset + "][" + level + "][" + latitude + "][" + longitude + "]";
+        } else if ( frame.altitude >= 25908 && frame.altitude < 44307 ) {
+            u_ext = "ugrd30_0mb[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+            v_ext = "vgrd30_0mb[" + gfs_offset + "][" + latitude + "][" + longitude + "]";
+        }
+    } else if ( model === "gfshd" ) {
+        latitude  = Math.floor((( frame.latitude + 90 ) / ( 180 )) * 360 );
+        longitude = Math.floor(( frame.longitude / 359.5 ) / 719 );
+        //modelURl =
+
+
+
+        //////////////////////////////////
+        // Altitude Level Determination //
+        //////////////////////////////////
+
     }
 
 
@@ -96,81 +179,81 @@ exports.wind = function( frame, time, model, table, cache, stats, parentCallback
     // Get Wind Files (Concurrently) //
     ///////////////////////////////////
     async.parallel({
-        u_wind: function( callback ) {          
-            if ( typeof cache[u_ext] !== "undefined" && cache[u_ext] !== '' ) {                
+        u_wind: function( callback ) {
+            if ( typeof cache[u_ext] !== "undefined" && cache[u_ext] !== '' ) {
                 //
                 // Pull from gradscache?
-                //  
+                //
                 callback( null, cache[u_ext] );
-                
+
                 stats.cacheHits++;
-            } else {                
+            } else {
                 u_url = url.parse(baseURL + modelURL + u_ext);
-                u_res = ""; 
-                
+                u_res = "";
+
                 u_req = http.get({
-                    hostname: u_url.hostname, 
+                    hostname: u_url.hostname,
                     path: u_url.path
                 }, function( response ) {
                     response.setEncoding('utf8');
-                    
+
                     response.on("data", function( data ) {
                         u_res += data
                     });
-                    
+
                     response.on("end", function() {
                        callback(null, u_res);
-                       
+
                        stats.gradsHits++;
                     });
                 }).on("error", function() {
-                    callback(true, null); 
+                    callback(true, null);
                 });
             }
         },
 
-        v_wind: function( callback ) {        
-            if ( typeof cache[v_ext] !== "undefined" && cache[v_ext] !== '' ) {                
+        v_wind: function( callback ) {
+            if ( typeof cache[v_ext] !== "undefined" && cache[v_ext] !== '' ) {
                 //
                 // Pull from gradscache?
-                //  
+                //
                 callback( null, cache[u_ext] );
-                
+
                 stats.cacheHits++;
             } else {
                 v_url = url.parse(baseURL + modelURL + v_ext);
                 v_res = "";
-                
+
                 v_req = http.get({
-                    hostname: v_url.hostname, 
+                    hostname: v_url.hostname,
                     path: v_url.path
                 }, function( response ) {
                     response.setEncoding('utf8');
-                    
+
                     response.on("data", function( data ) {
                         v_res += data
                     });
-                    
-                    response.on("end", function() {                   
+
+                    response.on("end", function() {
                         callback(null, v_res);
-                        
+
                         stats.gradsHits++;
                     });
                 }).on("error", function() {
-                   callback(true, null); 
+                   callback(true, null);
                 });
             }
         }
     }, function( error, results ) {
         if ( error ) {
             console.log("\033[0;31mgradsfail: Request Error (can you reach http://nomads.ncep.noaa.gov:9090/ ?)\033[0m");
-                
+
             parentCallback( error );
         } else {
             /////////////////////////////////////////////////////
             // Transform U & V Components to Wind Vector (m/s) //
             /////////////////////////////////////////////////////
-            
+
             if ( results.u_wind.indexOf("GrADS Data Server") !== -1 && results.v_wind.indexOf("GrADS Data Server") !== -1 ) {
                 //
                 // This piece catches a little bug called a "GrADS Fail."
@@ -178,7 +261,7 @@ exports.wind = function( frame, time, model, table, cache, stats, parentCallback
                 // I answer, wallowing in all my lost predictions.
                 //
                 console.log("\033[0;31mgradsfail: Unknown Error (probably time offset).\033[0m");
-                
+
                 parentCallback( true );
             } else {
                 cache[u_ext] = results.u_wind;
@@ -187,17 +270,17 @@ exports.wind = function( frame, time, model, table, cache, stats, parentCallback
                 v_wind = results.v_wind.substring( results.v_wind.indexOf("[0],") + 5, results.v_wind.indexOf("\n", 30));
                 u_wind = u_wind.trim();
                 v_wind = v_wind.trim();
-                
+
                 offset     = Math.atan2( v_wind, u_wind ) * degrees; // Is an offset from {below} value.
-                heading = ( 270 + offset ) - 180; // Proper direction.
-                speed       = Math.sqrt( Math.pow(Math.abs(v_wind), 2) + Math.pow(Math.abs(u_wind), 2) );
-                
+                heading    = ( 270 + offset ) - 180; // Proper direction - Pretty damned critical.
+                speed      = Math.sqrt( Math.pow(Math.abs(v_wind), 2) + Math.pow(Math.abs(u_wind), 2) );
+
                 // Can I be moved into primary loop via callback of any sort?
-                newPoints = position.travel(table[table.length - 2 ], speed * 60, heading);                           
+                newPoints = position.travel(table[table.length - 2 ], speed * 60, heading);
                 table[table.length - 1].latitude = newPoints[0];
                 table[table.length - 1].longitude = newPoints[1];
                 // end moveme
-                
+
                 parentCallback();
             }
         }
